1978983067cb0ceae37196379d9129bcdd2588f8
Raihan <raihan@gmail.com>
2017-08-29 17:27:41 +0600

    settings file modified

--- pyBlog/urls.py
+++ pyBlog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/$', views.home, name='home'),
     url(r'^single-post/(?P<post_id>[0-9]+)/', views.single_post, name='single-post'),
-    url(r'^cost/', include('cost.urls'), name="cost")
+    url(r'^cost/', include('cost.urls'), name="cost"),
+    url(r'^auth/', include("cAuth.urls")),
 ]
